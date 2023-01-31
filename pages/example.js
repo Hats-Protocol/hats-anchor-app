2d04678b4593eb7ba42619af8796012c72d49c32
nintynick <nicholas.naraghi@gmail.com>
2023-01-31 12:27:24 -0500

    Clean up UI components for view-hat (still working in example.js)

--- pages/example.js
+++ pages/example.js
@@ -34,93 +34,19 @@ const user = {
 }
 const navigation = [
   { name: 'Dashboard', href: '#' },
-  { name: 'Jobs', href: '#' },
-  { name: 'Applicants', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'View Tree', href: '#' },
+  { name: 'View Hat', href: '#' },
+  { name: 'Create Hat', href: '#' },
 ]
 const breadcrumbs = [
-  { name: 'Jobs', href: '#', current: false },
-  { name: 'Front End Developer', href: '#', current: false },
-  { name: 'Applicants', href: '#', current: true },
+  { name: 'Top Hat', href: '#', current: false },
+  { name: 'Marketing', href: '#', current: false },
+  { name: 'Marketing Manager', href: '#', current: true },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
-]
-const attachments = [
-  { name: 'resume_front_end_developer.pdf', href: '#' },
-  { name: 'coverletter_front_end_developer.pdf', href: '#' },
-]
-const eventTypes = {
-  applied: { icon: UserIcon, bgColorClass: 'bg-gray-400' },
-  advanced: { icon: HandThumbUpIcon, bgColorClass: 'bg-blue-500' },
-  completed: { icon: CheckIcon, bgColorClass: 'bg-green-500' },
-}
-const timeline = [
-  {
-    id: 1,
-    type: eventTypes.applied,
-    content: 'Applied to',
-    target: 'Front End Developer',
-    date: 'Sep 20',
-    datetime: '2020-09-20',
-  },
-  {
-    id: 2,
-    type: eventTypes.advanced,
-    content: 'Advanced to phone screening by',
-    target: 'Bethany Blake',
-    date: 'Sep 22',
-    datetime: '2020-09-22',
-  },
-  {
-    id: 3,
-    type: eventTypes.completed,
-    content: 'Completed phone screening with',
-    target: 'Martha Gardner',
-    date: 'Sep 28',
-    datetime: '2020-09-28',
-  },
-  {
-    id: 4,
-    type: eventTypes.advanced,
-    content: 'Advanced to interview by',
-    target: 'Bethany Blake',
-    date: 'Sep 30',
-    datetime: '2020-09-30',
-  },
-  {
-    id: 5,
-    type: eventTypes.completed,
-    content: 'Completed interview with',
-    target: 'Katherine Snyder',
-    date: 'Oct 4',
-    datetime: '2020-10-04',
-  },
-]
-const comments = [
-  {
-    id: 1,
-    name: 'Leslie Alexander',
-    date: '4d ago',
-    imageId: '1494790108377-be9c29b29330',
-    body: 'Ducimus quas delectus ad maxime totam doloribus reiciendis ex. Tempore dolorem maiores. Similique voluptatibus tempore non ut.',
-  },
-  {
-    id: 2,
-    name: 'Michael Foster',
-    date: '4d ago',
-    imageId: '1519244703995-f4e0f30006d5',
-    body: 'Et ut autem. Voluptatem eum dolores sint necessitatibus quos. Quis eum qui dolorem accusantium voluptas voluptatem ipsum. Quo facere iusto quia accusamus veniam id explicabo et aut.',
-  },
-  {
-    id: 3,
-    name: 'Dries Vincent',
-    date: '4d ago',
-    imageId: '1506794778202-cad84cf45f1d',
-    body: 'Expedita consequatur sit ea voluptas quo ipsam recusandae. Ab sint et voluptatem repudiandae voluptatem et eveniet. Nihil quas consequatur autem. Perferendis rerum et.',
-  },
 ]
 
 function classNames(...classes) {
@@ -136,25 +62,22 @@ const people = [
 
 function FullWidthTable(title) {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title, email and role.
-          </p>
+          <h1 className="text-lg font-medium text-gray-900">{title}</h1>
         </div>
         <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
           <button
             type="button"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
-            Add user
+            Add item
           </button>
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="mt-4 flex flex-col">
+        <div className="">
           <div className="inline-block min-w-full py-2 align-middle">
             <div className="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
               <table className="min-w-full divide-y divide-gray-300">
@@ -474,26 +397,44 @@ export default function Example() {
                 <section aria-labelledby="applicant-information-title">
                   <div className="bg-white shadow sm:rounded-lg">
                     <div className="px-4 py-5 sm:px-6">
-                      <h2 id="applicant-information-title" className="text-lg font-medium leading-6 text-gray-900">
-                        Applicant Information
-                    </h2>
-                      <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                      <h2 id="applicant-information-title" className="text-lg font-medium text-gray-900">
+                        Hat Description
+                      </h2>
                     </div>
                     <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
                       <dl className="">
                         <div>
-                          <dt className="text-sm font-medium text-gray-500">About</dt>
+                          <dt className="text-sm font-medium text-gray-500">Summary</dt>
                           <dd className="mt-1 text-sm text-gray-900">
-                            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
-                            Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                            proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
-                            Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                            proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
-                            Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                            proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                        </dd>
+                            Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et. Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et.
+                          </dd>
+                          <dt className="text-sm font-medium mt-4 text-gray-500">Responsibilities</dt>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Fugiat ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Aliquip ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dt className="text-sm font-medium mt-4 text-gray-500">Authorities</dt>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Aliquip ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Fugiat ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dt className="text-sm font-medium mt-4 text-gray-500">Qualifications</dt>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Fugiat ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
+                          <dd className="mt-1 text-sm text-gray-900">
+                            - Fugiat ipsum ipsum deserunt culpa aute sint do.
+                          </dd>
                         </div>
                       </dl>
                     </div>
@@ -502,7 +443,7 @@ export default function Example() {
                         href="#"
                         className="block bg-gray-50 px-4 py-4 text-center text-sm font-medium text-gray-500 hover:text-gray-700 sm:rounded-b-lg"
                       >
-                        Read full application
+                        Read full description
                     </a>
                     </div>
                   </div>
@@ -512,13 +453,9 @@ export default function Example() {
 
               <section aria-labelledby="timeline-title" className="lg:col-span-2 lg:col-start-2">
                 <div>
-                  <h1 className="text-2xl font-bold text-gray-900">Marketing Manager Hat</h1>
-                  <p className="text-sm font-medium text-gray-500">
-                    Applied for{' '}
-                    <a href="#" className="text-gray-900">
-                      Front End Developer
-                  </a>{' '}
-                  on <time dateTime="2020-08-25">August 25, 2020</time>
+                  <h1 className="text-3xl font-bold text-gray-900">Marketing Manager Hat</h1>
+                  <p className="text-sm font-medium my-2 text-gray-500">
+                    Hat ID 1.1 | 27065258958819196981364933114703301105956039517941121592357921226752
                   </p>
                 </div>
                 <div className="bg-white px-4 py-5 my-5 shadow sm:rounded-lg sm:px-6">
