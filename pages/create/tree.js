0c4ef731b4d2567599bc085f99a8fb8a5934a83c
Ido <gershido@gmail.com>
2023-02-15 12:30:36 +0200

    create tree page

--- pages/create/tree.js
+++ pages/create/tree.js
@@ -1,14 +1,52 @@
-import { useForm } from "react-hook-form";
+//import { useForm } from "react-hook-form";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import abi from "../../public/Hats.json";
+import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+console.log(JSON.stringify(abi));
 
 export default function CreateTree() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [values, setValues] = useState({
+    address: "",
+    details: "",
+    imageUri: "",
+  });
+
+  const { config } = usePrepareContractWrite({
+    address: "0x96bD657Fcc04c71B47f896a829E5728415cbcAa1",
+    abi: JSON.stringify(abi),
+    functionName: "mintTopHat",
+    args: [values.address, values.details, values.imageUri],
+    enabled:
+      values.address != "" && values.details != "" && values.imageUri != "",
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    write?.();
+    console.log(values);
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="bg-slate-50 flex-grow ">
       <div className="max-w-screen-xl m-auto">
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit}
           className="mt-8 rounded-md shadow-md bg-white"
         >
           <div className="space-y-6 mx-4 flex flex-col">
@@ -24,20 +62,20 @@ export default function CreateTree() {
 
             <div className="">
               <label
-                htmlFor="tree-name"
+                htmlFor="address"
                 className="block text-sm font-medium text-gray-700"
               >
                 Top-Hat Address
               </label>
               <input
                 type="text"
-                name="tree-name"
-                id="tree-name"
+                name="address"
+                id="address"
                 className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 text-sm mt-2"
                 placeholder="0x123..."
-                {...register("topHatAddress")}
+                onChange={onChange}
               />
-              <p className="mt-2 text-sm text-gray-500">
+              <p className="mt-2 text-sm text-gray-500 in">
                 Address of the top most admin, aka the top-hat.
               </p>
             </div>
@@ -56,7 +94,7 @@ export default function CreateTree() {
                   rows={3}
                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder="Tree description"
-                  {...register("details")}
+                  onChange={onChange}
                 />
               </div>
               <p className="mt-2 text-sm text-gray-500">
@@ -66,18 +104,18 @@ export default function CreateTree() {
 
             <div className="">
               <label
-                htmlFor="image-uri"
+                htmlFor="imageUri"
                 className="block text-sm font-medium text-gray-700"
               >
                 Image URI
               </label>
               <input
                 type="text"
-                name="image-uri"
-                id="image-uri"
+                name="imageUri"
+                id="imageUri"
                 className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 text-sm mt-2"
                 placeholder="https://ipfs.io/ipfs/..."
-                {...register("imageURI")}
+                onChange={onChange}
               />
               <p className="mt-2 text-sm text-gray-500">
                 What image do you want to represent this tree? This will be the
@@ -89,9 +127,14 @@ export default function CreateTree() {
             <div>
               <button
                 type="submit"
+                disabled={!write}
                 className="mb-2 rounded-md border border-transparent bg-indigo-600 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                <div className="m-2">Create</div>
+                {isLoading ? (
+                  <ClipLoader loading={isLoading} />
+                ) : (
+                  <div className="m-2">Create</div>
+                )}
               </button>
             </div>
           </div>
