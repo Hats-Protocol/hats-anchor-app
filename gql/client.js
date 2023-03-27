10d2ad45f2b246cb5cddcffccf1e9e0eae032833
Scott Herren <me@stwoh.com>
2023-03-27 07:05:38 -0500

    update to v1

--- gql/client.js
+++ gql/client.js
@@ -3,10 +3,15 @@ import { GraphQLClient } from 'graphql-request';
 // TODO need mainnet subgraph
 const subgraphUrl = (chainId) => {
   const clients = {
-    // 1: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-mainnet',
-    5: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-goerli',
-    100: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-gnosis-chain',
-    137: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-polygon',
+    1: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-mainnet',
+    5: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-goerli',
+    10: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-optimism',
+    100: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-gnosis-chain',
+    137: 'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-polygon',
+    42161:
+      'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-arbitrum',
+    11155111:
+      'https://api.thegraph.com/subgraphs/name/hats-protocol/hats-protocol-v1-sepolia',
   };
 
   return clients[chainId] || clients[5];
