b460d711de84211089d948ffa9c97e12762610b4
vidvidvid <weetopol@gmail.com>
2023-05-01 15:21:22 +0200

    extract AddressRow from Hat

--- components/Hat/Hat.jsx
+++ components/Hat/Hat.jsx
@@ -13,23 +13,18 @@ import {
   IconButton,
   Button,
   Box,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Tooltip,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import _ from 'lodash';
 import { formatDistanceToNow } from 'date-fns';
-import { FaPencilAlt, FaEllipsisV, FaExternalLinkAlt } from 'react-icons/fa';
+import { FaPencilAlt, FaExternalLinkAlt } from 'react-icons/fa';
 import { useAccount, useChainId } from 'wagmi';
 
-import Link from '../ChakraNextLink';
 import HatWearers from './HatWearers';
-import AddressLink from '../AddressLink';
+import AddressRow from './AddressRow';
+import Link from '../ChakraNextLink';
 import DataTable from '../DataTable';
-import { ZERO_ADDRESS, MODULE_TYPES, hatsAddresses } from '../../constants';
+import { MODULE_TYPES, hatsAddresses } from '../../constants';
 import Modal from '../Modal';
 import HatModulesForm from '../../forms/HatModulesForm';
 import { useOverlay } from '../../contexts/OverlayContext';
@@ -59,98 +54,6 @@ import HatUnlinkForm from '../../forms/HatUnlinkForm';
 
 const defaultChainId = 5;
 const hatsAddress = hatsAddresses(defaultChainId);
-
-const AddressRow = ({
-  address,
-  mutable,
-  admin,
-  chainId,
-  type,
-  setType,
-  localOverlay,
-  checkHatStatus,
-}) => {
-  const { address: userAddress } = useAccount();
-  const { setModals } = localOverlay;
-
-  const openEditModal = () => {
-    setType(type);
-    setModals({ editModule: true });
-  };
-
-  const openWearerStatusModal = () => {
-    setModals({ hatWearerStatus: true });
-  };
-
-  const openHatStatusModal = () => {
-    setModals({ hatStatus: true });
-  };
-
-  const handleCheckHatStatus = async () => {
-    await checkHatStatus?.();
-  };
-
-  return (
-    <HStack spacing={3}>
-      {address !== ZERO_ADDRESS ? (
-        <AddressLink address={address} chainId={chainId} />
-      ) : (
-        <Text>None Set</Text>
-      )}
-
-      {userAddress && ((mutable && admin) || userAddress === address) && (
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            icon={<Icon as={FaEllipsisV} h='12px' w='12px' />}
-            minW='auto'
-            w={8}
-            h={8}
-            variant='ghost'
-          />
-          <MenuList>
-            {mutable && admin && (
-              <MenuItem onClick={openEditModal}>
-                Edit {_.capitalize(type)} Module
-              </MenuItem>
-            )}
-            {mutable &&
-              _.eq(type, MODULE_TYPES.eligibility) &&
-              _.eq(_.toLower(userAddress), _.toLower(address)) && (
-                <MenuItem onClick={openWearerStatusModal}>
-                  Change Wearer Status
-                </MenuItem>
-              )}
-            {mutable && _.eq(type, MODULE_TYPES.toggle) && (
-              <>
-                <MenuItem
-                  onClick={handleCheckHatStatus}
-                  isDisabled={!checkHatStatus}
-                >
-                  <Tooltip
-                    label={!checkHatStatus ? 'Toggle is not a contract' : ''}
-                    placement='left'
-                    bg='gray.100'
-                    color='black'
-                    hasArrow
-                  >
-                    Check Hat Status
-                  </Tooltip>
-                </MenuItem>
-
-                {_.eq(_.toLower(userAddress), _.toLower(address)) && (
-                  <MenuItem onClick={openHatStatusModal}>
-                    Change Hat Status
-                  </MenuItem>
-                )}
-              </>
-            )}
-          </MenuList>
-        </Menu>
-      )}
-    </HStack>
-  );
-};
 
 // TODO this should probably be more components
 
