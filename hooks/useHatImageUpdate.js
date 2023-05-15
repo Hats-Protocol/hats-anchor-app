9098c55463722837cdc35edcadb2efb84d6828b0
Ido <gershido@gmail.com>
2023-05-15 15:51:56 +0300

    add custom image in image edit form

--- hooks/useHatImageUpdate.js
+++ hooks/useHatImageUpdate.js
@@ -4,10 +4,13 @@ import { hatsAddresses, ZERO_ADDRESS } from '../constants';
 import abi from '../contracts/Hats.json';
 import useToast from './useToast';
 import { useOverlay } from '../contexts/OverlayContext';
+import { useQueryClient } from '@tanstack/react-query';
+import { idToPrettyId, prettyIdToIp, toTreeId } from '../lib/hats';
 
 const useHatImageUpdate = ({ hatsAddress, chainId, hatId, image }) => {
   const { handlePendingTx } = useOverlay();
   const toast = useToast();
+  const queryClient = useQueryClient();
 
   const { config } = usePrepareContractWrite({
     address: hatsAddress || hatsAddresses(chainId),
@@ -23,19 +26,28 @@ const useHatImageUpdate = ({ hatsAddress, chainId, hatId, image }) => {
 
   const { writeAsync } = useContractWrite({
     ...config,
-    onSuccess: (data) => {
-      handlePendingTx({
-        hash: _.get(data, 'hash'),
-        toastData: {
-          title: 'Image updated!',
-          description: `Successfully updated the image for hat #${hatId}`,
-        },
-      });
-
+    onSuccess: async (data) => {
       toast.info({
         title: 'Transaction submitted',
         description: 'Waiting for your transaction to be accepted...',
       });
+
+      handlePendingTx({
+        hash: _.get(data, 'hash'),
+        toastData: {
+          title: 'Image updated!',
+          description: `Successfully updated the image for hat #${prettyIdToIp(
+            idToPrettyId(hatId),
+          )}`,
+        },
+      });
+
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: ['hatDetails', hatId] });
+        queryClient.invalidateQueries({
+          queryKey: ['treeDetails', toTreeId(hatId)],
+        });
+      }, 4000);
     },
     onError: (error) => {
       if (error.name === 'UserRejectedRequestError') {
