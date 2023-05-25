9ba2e05b7ba795e9d983fb5867a420f8dfddc60c
vidvidvid <weetopol@gmail.com>
2023-05-25 09:07:14 +0200

    support ens in hat wearer form

--- forms/HatWearerForm.jsx
+++ forms/HatWearerForm.jsx
@@ -11,10 +11,12 @@ import {
   InputLeftElement,
   InputRightElement,
   Text,
+  Box,
 } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { isAddress } from 'viem';
 import { FaCheck, FaTrash, FaUserPlus } from 'react-icons/fa';
+import { useEnsAddress } from 'wagmi';
 import useHatMint from '../hooks/useHatMint';
 import useDebounce from '../hooks/useDebounce';
 import CONFIG from '../constants';
@@ -27,15 +29,9 @@ const HatWearerForm = ({ hatId, chainId, currentWearers, maxSupply }) => {
   const [newAddress, setNewAddress] = useState('');
 
   const newWearer = useDebounce(newAddress, CONFIG.debounce);
-  const isNewWearerAddress = isAddress(newWearer);
   const isAddressAlreadyAdded =
     wearers.some((wearer) => wearer.address === newAddress) ||
     currentWearers.includes(newAddress);
-
-  const wouldExceedMaxSupply =
-    currentWearers.length + wearers.length + 1 > maxSupply;
-  const canAddWearer =
-    isNewWearerAddress && !isAddressAlreadyAdded && !wouldExceedMaxSupply;
 
   const { writeAsync, isLoading } = useHatMint({
     hatsAddress: CONFIG.hatsAddress,
@@ -48,11 +44,21 @@ const HatWearerForm = ({ hatId, chainId, currentWearers, maxSupply }) => {
     await writeAsync?.();
   };
 
+  const { data: ensResolvedAddress, isSuccess: isEnsAddress } = useEnsAddress({
+    name: newAddress,
+    chainId: 1,
+  });
+
+  const isNewWearerAddress = isAddress(newWearer) || ensResolvedAddress;
+  const wouldExceedMaxSupply =
+    currentWearers.length + wearers.length + 1 > maxSupply;
+  const canAddWearer =
+    isNewWearerAddress && !isAddressAlreadyAdded && !wouldExceedMaxSupply;
+
   const handleAddWearer = () => {
-    if (isNewWearerAddress && !isAddressAlreadyAdded) {
-      setWearers([...wearers, { address: newAddress }]);
-      setNewAddress('');
-    }
+    const address = isAddress(newWearer) ? newWearer : ensResolvedAddress;
+    setWearers([...wearers, { address, ens: isEnsAddress && newWearer }]);
+    setNewAddress('');
   };
 
   const handleRemoveWearer = (index) => {
@@ -71,62 +77,66 @@ const HatWearerForm = ({ hatId, chainId, currentWearers, maxSupply }) => {
               <FaUserPlus ml={2} />
             </InputLeftElement>
             <Input
-              pl={16}
-              pr={3}
-              w='calc(100% - 7rem)'
+              w='calc(100% - 1rem)'
               textOverflow='ellipsis'
               type='address'
-              placeholder='Paste or write an ETH address...'
+              placeholder='0x1234, vitalik.eth'
               value={newAddress}
               onChange={(e) =>
                 setNewAddress(e.target.value?.toLowerCase() ?? '')
               }
+              rightElement={ensResolvedAddress && <FaCheck color='green' />}
             />
-            <InputRightElement w='4rem'>
-              <Tooltip
-                label={
-                  !canAddWearer
-                    ? !isNewWearerAddress
-                      ? 'Please input a valid address'
-                      : isAddressAlreadyAdded
-                      ? 'Address already added'
-                      : wouldExceedMaxSupply
-                      ? 'Max supply would be exceeded'
-                      : ''
-                    : ''
-                }
-                shouldWrapChildren
-              >
-                <IconButton
-                  isDisabled={!canAddWearer}
-                  onClick={handleAddWearer}
-                  icon={<FaCheck />}
-                  aria-label='Add'
-                  height={9}
-                  w={16}
-                />
-              </Tooltip>
-            </InputRightElement>
+            {ensResolvedAddress && (
+              <InputRightElement right='2rem'>
+                <FaCheck color='green' />
+              </InputRightElement>
+            )}
           </InputGroup>
-        </Flex>
-
-        {wearers.map((wearer, index) => (
-          <Flex
-            key={wearer}
-            align='center'
-            w='full'
-            justifyContent='space-between'
+          <Tooltip
+            label={
+              !canAddWearer
+                ? !isNewWearerAddress
+                  ? 'Please input a valid address'
+                  : isAddressAlreadyAdded
+                  ? 'Address already added'
+                  : wouldExceedMaxSupply
+                  ? 'Max supply would be exceeded'
+                  : ''
+                : ''
+            }
+            shouldWrapChildren
           >
-            <Input value={wearer.address} readOnly w='calc(100% - 7rem)' />
             <IconButton
-              type='button'
-              onClick={() => handleRemoveWearer(index)}
-              icon={<FaTrash />}
-              aria-label='Remove'
+              isDisabled={!canAddWearer}
+              onClick={handleAddWearer}
+              icon={<FaCheck />}
+              aria-label='Add'
               height={9}
               w={16}
             />
-          </Flex>
+          </Tooltip>
+        </Flex>
+
+        {wearers.map(({ address, ens }, index) => (
+          <Box key={address}>
+            <Flex align='center' w='full' justifyContent='space-between'>
+              <Input value={ens || address} readOnly w='calc(100% - 5rem)' />
+              <IconButton
+                type='button'
+                onClick={() => handleRemoveWearer(index)}
+                icon={<FaTrash />}
+                aria-label='Remove'
+                height={9}
+                w={16}
+              />
+            </Flex>
+            {ens && (
+              <Text fontSize='sm' color='gray.500' mt={1}>
+                Resolved address: {address}
+              </Text>
+            )}
+          </Box>
         ))}
 
         <Flex justify='flex-end'>
