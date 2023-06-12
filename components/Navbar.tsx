a05a029d46eb85b6b47c8bf3b9a4059b8fc0ef30
Scott Herren <me@stwoh.com>
2023-06-12 16:09:38 -0500

    clean up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,9 +16,7 @@ import { useAccount } from 'wagmi';
 
 import ChakraNextLink from '@/components/ChakraNextLink';
 import ConnectWallet from '@/components/ConnectWallet';
-import Modal from '@/components/Modal';
 import { useOverlay } from '@/contexts/OverlayContext';
-import TreeCreateForm from '@/forms/TreeCreateForm';
 import CONFIG from '@/constants';
 import { fetchHatDetails } from '@/gql/helpers';
 import { fetchDetailsIpfs } from '@/hooks/useHatDetailsField';
@@ -27,10 +25,8 @@ import { urlIdToPrettyId, prettyIdToId } from '@/lib/hats';
 const Navbar = () => {
   const localOverlay = useOverlay();
   const { setCommandPallet: setOpen } = localOverlay;
-  // const { setModals } = localOverlay;
   const router = useRouter();
   const path = router.asPath.split('/').slice(1);
-  console.log(path);
   const { address } = useAccount();
   const [currentTopHatName, setCurrentTopHatName] = useState<any>(null);
 
@@ -42,7 +38,6 @@ const Navbar = () => {
       if (!topHatId) {
         return;
       }
-      console.log(topHatId);
       const topHat = await fetchHatDetails(
         prettyIdToId(topHatId),
         Number(chainId),
@@ -54,103 +49,78 @@ const Navbar = () => {
       } else {
         setCurrentTopHatName(_.get(topHat, 'details'));
       }
-      console.log(topHat);
     };
 
     if (!currentTopHatName && path) {
       getTopHatDetails();
     }
   }, [path, currentTopHatName]);
-  console.log(currentTopHatName);
-
-  // const navLinks = useMemo(() => {
-  //   const links = [
-  //     { name: _.capitalize(CONFIG.trees), href: '/trees' },
-  //     // {
-  //     //   name: 'Create Tree',
-  //     //   onClick: () => setModals?.({ createTree: true }),
-  //     // },
-  //     address && {
-  //       name: `My ${_.capitalize(CONFIG.hats)}`,
-  //       href: `/${CONFIG.wearers}/${address}`,
-  //     },
-  //   ];
-
-  //   return clearNonObjects(links);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [address]);
 
   return (
-    <>
-      <Modal name='createTree' title='Create Tree' localOverlay={localOverlay}>
-        <TreeCreateForm />
-      </Modal>
-
-      <Flex
-        w='100%'
-        justify='space-between'
-        align='center'
-        px={8}
-        bg='white'
-        borderBottom='1px solid'
-        borderColor='gray.400'
-        boxShadow='md'
-        position='fixed'
-        zIndex={10}
-        minH='75px'
-      >
-        <HStack spacing={6}>
-          <ChakraNextLink href='/'>
-            <Image src='/icon.jpeg' h='70px' alt='Hats Logo' />
+    <Flex
+      w='100%'
+      justify='space-between'
+      align='center'
+      px={8}
+      bg='white'
+      borderBottom='1px solid'
+      borderColor='gray.400'
+      boxShadow='md'
+      position='fixed'
+      zIndex={10}
+      minH='75px'
+    >
+      <HStack spacing={6}>
+        <ChakraNextLink href='/'>
+          <Image src='/icon.jpeg' h='70px' alt='Hats Logo' />
+        </ChakraNextLink>
+        <HStack spacing={5}>
+          <ChakraNextLink href={`/${CONFIG.trees}`}>
+            <Button
+              h='75px'
+              minW='125px'
+              variant='ghost'
+              borderRadius={0}
+              _active={{ borderBottom: '2px solid', bg: 'gray.100' }}
+              isActive={_.includes(path, CONFIG.trees)}
+            >
+              {!currentTopHatName ? (
+                <Text fontSize='lg'>{_.capitalize(CONFIG.trees)}</Text>
+              ) : (
+                <Stack align='start' w='90%' mx={3}>
+                  <Text fontSize='sm'>{_.toUpper(CONFIG.trees)}</Text>
+                  <Text fontSize='lg' color='gray.500'>
+                    {_.capitalize(currentTopHatName)}
+                  </Text>
+                </Stack>
+              )}
+            </Button>
           </ChakraNextLink>
-          <HStack spacing={5}>
-            <ChakraNextLink href={`/${CONFIG.trees}`}>
-              <Button
-                h='75px'
-                minW='125px'
-                variant='ghost'
-                borderRadius={0}
-                _active={{ borderBottom: '2px solid', bg: 'gray.100' }}
-                isActive={_.includes(path, CONFIG.trees)}
-              >
-                {!currentTopHatName ? (
-                  <Text fontSize='lg'>{_.capitalize(CONFIG.trees)}</Text>
-                ) : (
-                  <Stack align='start' w='90%' mx={3}>
-                    <Text fontSize='sm'>{_.toUpper(CONFIG.trees)}</Text>
-                    <Text fontSize='lg' color='gray.500'>
-                      {_.capitalize(currentTopHatName)}
-                    </Text>
-                  </Stack>
-                )}
-              </Button>
-            </ChakraNextLink>
-            <ChakraNextLink href={`/${CONFIG.wearers}/${address}`}>
-              <Button
-                h='75px'
-                minW='100px'
-                variant='ghost'
-                borderRadius={0}
-                fontSize='lg'
-                _active={{ borderBottom: '2px solid', bg: 'gray.100' }}
-                isActive={_.includes(path, address)}
-              >
-                {`My ${_.capitalize(CONFIG.hats)}`}
-              </Button>
-            </ChakraNextLink>
-          </HStack>
+          <ChakraNextLink href={`/${CONFIG.wearers}/${address}`}>
+            <Button
+              h='75px'
+              minW='100px'
+              variant='ghost'
+              borderRadius={0}
+              fontSize='lg'
+              _active={{ borderBottom: '2px solid', bg: 'gray.100' }}
+              isActive={_.includes(path, address)}
+            >
+              {`My ${_.capitalize(CONFIG.hats)}`}
+            </Button>
+          </ChakraNextLink>
         </HStack>
+      </HStack>
 
-        <HStack spacing={2}>
-          <IconButton
-            icon={<Icon as={FaSearch} h='25px' w='25px' />}
-            onClick={() => setOpen?.(true)}
-            aria-label='Search'
-          />
-          <ConnectWallet />
-        </HStack>
-      </Flex>
-    </>
+      <HStack spacing={2}>
+        <IconButton
+          icon={<Icon as={FaSearch} h='25px' w='25px' />}
+          onClick={() => setOpen?.(true)}
+          aria-label='Search'
+        />
+        <ConnectWallet />
+      </HStack>
+    </Flex>
   );
 };
 
