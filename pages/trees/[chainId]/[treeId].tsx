86429dba48957068269a6fbb13863b0a1a545392
vidvidvid <weetopol@gmail.com>
2023-07-07 17:37:09 +0200

    events popover

--- pages/trees/[chainId]/[treeId].tsx
+++ pages/trees/[chainId]/[treeId].tsx
@@ -2,12 +2,12 @@ import {
   Box,
   Button,
   Checkbox,
-  Divider,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerOverlay,
   Flex,
+  Heading,
   HStack,
   Icon,
   IconButton,
@@ -15,6 +15,7 @@ import {
   Popover,
   PopoverArrow,
   PopoverBody,
+  PopoverCloseButton,
   PopoverContent,
   PopoverTrigger,
   Radio,
@@ -24,14 +25,22 @@ import {
   Text,
   useDisclosure,
   VStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Divider,
 } from '@chakra-ui/react';
 import { formatDistanceToNow } from 'date-fns';
 import _ from 'lodash';
 import dynamic from 'next/dynamic';
 import { NextSeo } from 'next-seo';
-import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { BsToggles } from 'react-icons/bs';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import { FaChevronDown, FaChevronUp, FaExternalLinkAlt } from 'react-icons/fa';
 import { FiExternalLink } from 'react-icons/fi';
 import { useAccount } from 'wagmi';
 import { useRouter } from 'next/router';
@@ -43,7 +52,7 @@ import CONFIG from '@/constants';
 import { fetchHatDetails, fetchTreeDetails } from '@/gql/helpers';
 import useImageURIs from '@/hooks/useImageURIs';
 import useWearerDetails from '@/hooks/useWearerDetails';
-import { mapWithChainId } from '@/lib/general';
+import { explorerUrl, mapWithChainId } from '@/lib/general';
 import {
   decimalId,
   decimalToTreeId,
@@ -148,6 +157,12 @@ const TreeDetails = ({
     onOpen: onOpenShade,
     onClose: onCloseShade,
     isOpen: isOpenShade,
+  } = useDisclosure();
+
+  const {
+    isOpen: isEventsModalOpen,
+    onOpen: handleOpenModal,
+    onClose: handleCloseModal,
   } = useDisclosure();
 
   const handleSelectHat = (id: string) => {
@@ -380,16 +395,56 @@ const TreeDetails = ({
                 </ChakraNextLink>
               </Flex>
               {!_.isEmpty(events) && (
-                <Flex align='center' gap={1} fontSize='sm'>
-                  <Text>Last event: </Text>
-                  <Text mr={2} fontWeight={500}>
-                    {formatDistanceToNow(
-                      new Date(Number(events[0]?.timestamp) * 1000),
-                    )}{' '}
-                    ago
-                  </Text>
-                  <Image src='/icons/ago.svg' alt='History icon' />
-                </Flex>
+                <Popover trigger='hover'>
+                  <PopoverTrigger>
+                    <Flex align='center' gap={1} fontSize='sm' cursor='pointer'>
+                      <Text>Last event: </Text>
+                      <Text mr={2} fontWeight={500}>
+                        {formatDistanceToNow(
+                          new Date(Number(events[0]?.timestamp) * 1000),
+                        )}{' '}
+                        ago
+                      </Text>
+                      <Image src='/icons/ago.svg' alt='History icon' />
+                    </Flex>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverBody>
+                      <Stack>
+                        <Heading
+                          size='sm'
+                          fontWeight='medium'
+                          textTransform='uppercase'
+                        >
+                          Event history
+                        </Heading>
+                        <Box>
+                          {treeData.events?.slice(0, 5).map((event: any) => (
+                            <Event
+                              key={`${event?.transactionID}-${event?.id}`}
+                              event={event}
+                              chainId={chainId}
+                            />
+                          ))}
+                          {treeData.events?.length > 4 && (
+                            <>
+                              <Divider my={2} />
+                              <Button
+                                onClick={handleOpenModal}
+                                variant='link'
+                                colorScheme='blue'
+                              >
+                                View Full History
+                              </Button>
+                            </>
+                          )}
+                        </Box>
+                      </Stack>
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
               )}
             </VStack>
           </Flex>
@@ -412,7 +467,55 @@ const TreeDetails = ({
           </Flex>
         )}
       </Layout>
+
+      <Modal isOpen={isEventsModalOpen} onClose={handleCloseModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Event History</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {treeData?.events?.map((event: any) => (
+              <Event
+                key={`${event?.transactionID}-${event?.id}`}
+                event={event}
+                chainId={chainId}
+              />
+            ))}
+          </ModalBody>
+          <ModalFooter>
+            <Button variant='ghost' onClick={handleCloseModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
+  );
+};
+
+const Event = ({ event, chainId }: { event: any; chainId: number }) => {
+  return (
+    <Flex
+      key={`${event?.transactionID}-${event?.id}`}
+      align='center'
+      justify='space-between'
+      py={2}
+    >
+      <Text>{`${formatDistanceToNow(
+        new Date(Number(event?.timestamp) * 1000),
+      )} ago`}</Text>
+
+      <ChakraNextLink
+        isExternal
+        href={`${explorerUrl(chainId)}/tx/${event?.transactionID}`}
+        display='block'
+      >
+        <HStack spacing={3}>
+          <Text>{event?.id?.split('-')[0]}</Text>
+          <Icon as={FaExternalLinkAlt} w='12px' color='blue.500' />
+        </HStack>
+      </ChakraNextLink>
+    </Flex>
   );
 };
 
