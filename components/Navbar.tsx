56dc2ba031ca659f58f3c1c28acda7a1762d08c6
Scott Herren <me@stwoh.com>
2023-07-12 10:16:40 -0500

    fix truncate

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -104,9 +104,9 @@ const Navbar = () => {
               {!currentTopHatName ? (
                 <Text fontSize='lg'>{_.capitalize(CONFIG.trees)}</Text>
               ) : (
-                <Stack align='start' w='90%' mx={3}>
+                <Stack align='start' w='90%' mx={2}>
                   <Text fontSize='sm'>{_.toUpper(CONFIG.trees)}</Text>
-                  <Text fontSize='lg' color='gray.500' isTruncated>
+                  <Text fontSize='lg' color='gray.500' isTruncated maxW='170px'>
                     {containsUpperCase(currentTopHatName)
                       ? currentTopHatName
                       : _.capitalize(currentTopHatName)}
