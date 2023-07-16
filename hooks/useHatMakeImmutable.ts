d2614a5d4aee22886691cc3be62b905fba08328f
vidvidvid <weetopol@gmail.com>
2023-07-16 18:09:42 +0200

    refactor useHatMakeImmutable

--- hooks/useHatMakeImmutable.ts
+++ hooks/useHatMakeImmutable.ts
@@ -1,17 +1,8 @@
-import { useQueryClient } from '@tanstack/react-query';
 import _ from 'lodash';
-import { useState } from 'react';
-import {
-  useContractWrite,
-  usePrepareContractWrite,
-  useWaitForTransaction,
-} from 'wagmi';
 
-import CONFIG from '@/constants';
-import { useOverlay } from '@/contexts/OverlayContext';
-import abi from '@/contracts/Hats.json';
-import useToast from '@/hooks/useToast';
 import { decimalId, idToPrettyId, prettyIdToIp, toTreeId } from '@/lib/hats';
+
+import useHatContractWrite from './useHatContractWrite';
 
 const useHatMakeImmutable = ({
   hatsAddress,
@@ -20,70 +11,25 @@ const useHatMakeImmutable = ({
   levelAtLocalTree,
   isAdminUser,
 }: UseHatMakeImmutableProps) => {
-  const toast = useToast();
-  const { handlePendingTx } = useOverlay();
-  const queryClient = useQueryClient();
-  const [hash, setHash] = useState<`0x${string}`>();
-
-  const { config } = usePrepareContractWrite({
-    address: hatsAddress || CONFIG.hatsAddress,
-    chainId: Number(chainId),
-    abi,
+  const { writeAsync, isLoading } = useHatContractWrite({
     functionName: 'makeHatImmutable',
     args: [decimalId(hatId)],
+    chainId: Number(chainId),
+    onSuccessToastData: {
+      title: 'Hat Updated!',
+      description: `Successfully made hat #${prettyIdToIp(
+        idToPrettyId(hatId),
+      )} immutable`,
+    },
+    queryKeys: [
+      ['hatDetails', hatId],
+      ['treeDetails', toTreeId(hatId)],
+    ],
     enabled:
-      !!hatsAddress &&
-      !!decimalId(hatId) &&
+      Boolean(hatsAddress) &&
+      Boolean(decimalId(hatId)) &&
       _.gt(levelAtLocalTree, 0) &&
       isAdminUser,
-  });
-
-  const { writeAsync } = useContractWrite({
-    ...config,
-    onSuccess: async (data) => {
-      setHash(data.hash);
-
-      toast.info({
-        title: 'Transaction submitted',
-        description: 'Waiting for your transaction to be accepted...',
-      });
-
-      await handlePendingTx({
-        hash: data.hash,
-        toastData: {
-          title: 'Hat Updated!',
-          description: `Successfully made hat #${prettyIdToIp(
-            idToPrettyId(hatId),
-          )} immutable`,
-        },
-      });
-
-      setTimeout(() => {
-        queryClient.invalidateQueries({
-          queryKey: ['hatDetails', hatId],
-        });
-        queryClient.invalidateQueries({
-          queryKey: ['treeDetails', toTreeId(hatId)],
-        });
-      }, 4000);
-    },
-    onError: (error) => {
-      if (error.name === 'UserRejectedRequestError') {
-        toast.error({
-          title: 'Signature rejected!',
-          description: 'Please accept the transaction in your wallet',
-        });
-      } else {
-        toast.error({
-          title: 'Error occurred!',
-          // description: 'Please accept the transaction in your wallet',
-        });
-      }
-    },
-  });
-
-  const { isLoading } = useWaitForTransaction({
-    hash,
   });
 
   return { writeAsync, isLoading };
