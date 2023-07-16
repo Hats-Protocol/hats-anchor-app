d4be1b1b9e1036aec8afa86bcc6bcf12ec677254
vidvidvid <weetopol@gmail.com>
2023-07-16 16:03:21 +0200

    set up useHatContractWrite

---
+++ hooks/useHatContractWrite.ts
@@ -0,0 +1,95 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from 'wagmi';
+
+import CONFIG from '@/constants';
+import { useOverlay } from '@/contexts/OverlayContext';
+import abi from '@/contracts/Hats.json';
+import useToast from '@/hooks/useToast';
+
+interface ContractInteractionProps {
+  functionName: string;
+  args: any[];
+  chainId: number;
+  onSuccessToastData: { title: string; description?: string };
+  onErrorToastData?: { title: string; description?: string };
+  queryKeys: string[][];
+  transactionTimeout?: number;
+  enabled: boolean;
+}
+
+const useHatContractWrite = ({
+  functionName,
+  args,
+  chainId,
+  onSuccessToastData,
+  onErrorToastData,
+  queryKeys,
+  transactionTimeout = 4000,
+  enabled,
+}: ContractInteractionProps) => {
+  const toast = useToast();
+  const { handlePendingTx } = useOverlay();
+  const queryClient = useQueryClient();
+  const [hash, setHash] = useState<`0x${string}`>();
+
+  const { config } = usePrepareContractWrite({
+    address: CONFIG.hatsAddress,
+    chainId: Number(chainId),
+    abi,
+    functionName,
+    args,
+    enabled,
+  });
+
+  const { writeAsync } = useContractWrite({
+    ...config,
+    onSuccess: async (data) => {
+      setHash(data.hash);
+      toast.info({
+        title: 'Transaction submitted',
+        description: 'Waiting for your transaction to be accepted...',
+      });
+
+      await handlePendingTx({
+        hash: data.hash,
+        toastData: onSuccessToastData,
+      });
+
+      setTimeout(() => {
+        queryKeys.forEach((key) =>
+          queryClient.invalidateQueries({
+            queryKey: key,
+          }),
+        );
+      }, transactionTimeout);
+    },
+    onError: (error) => {
+      if (error.name === 'UserRejectedRequestError') {
+        toast.error({
+          title: 'Signature rejected!',
+          description: 'Please accept the transaction in your wallet',
+        });
+      } else {
+        toast.error({
+          title: 'Error occurred!',
+          description:
+            onErrorToastData?.description ??
+            'An error occurred while processing the transaction.',
+        });
+      }
+    },
+  });
+
+  const { isLoading } = useWaitForTransaction({
+    hash,
+  });
+
+  return { writeAsync, isLoading };
+};
+
+export default useHatContractWrite;
--- hooks/useHatUnlinkTree.ts
+++ hooks/useHatUnlinkTree.ts
@@ -1,77 +1,36 @@
 import _ from 'lodash';
-import { useState } from 'react';
 import { isAddress } from 'viem';
-import {
-  useContractWrite,
-  usePrepareContractWrite,
-  useWaitForTransaction,
-} from 'wagmi';
 
-import CONFIG from '@/constants';
-import { useOverlay } from '@/contexts/OverlayContext';
-import abi from '@/contracts/Hats.json';
-import useToast from '@/hooks/useToast';
+import useHatContractWrite from '@/hooks/useHatContractWrite';
 import { prettyIdToIp } from '@/lib/hats';
+
+interface UseHatUnlinkTreeProps {
+  topHatPrettyId: string;
+  wearer: string;
+  chainId: number;
+}
 
 const useHatUnlinkTree = ({
   topHatPrettyId,
   wearer,
   chainId,
-}: {
-  topHatPrettyId: string;
-  wearer: string;
-  chainId: number;
-}) => {
-  const toast = useToast();
-  const { handlePendingTx } = useOverlay();
-  const [hash, setHash] = useState<`0x${string}`>();
-
-  const { config } = usePrepareContractWrite({
-    address: CONFIG.hatsAddress,
-    chainId,
-    abi,
+}: UseHatUnlinkTreeProps) => {
+  const { writeAsync, isLoading } = useHatContractWrite({
     functionName: 'unlinkTopHatFromTree',
     args: [topHatPrettyId, wearer],
+    chainId,
+    onSuccessToastData: {
+      title: `Top Hat Unlinked!`,
+      description: `Successfully unlinked top hat #${prettyIdToIp(
+        topHatPrettyId,
+      )}`,
+    },
+    onErrorToastData: {
+      title: 'Error occurred!',
+    },
+    queryKeys: [['topHat', topHatPrettyId]],
+    transactionTimeout: 4000,
     enabled: Boolean(topHatPrettyId) && Boolean(wearer) && isAddress(wearer),
-  });
-
-  const { writeAsync } = useContractWrite({
-    ...config,
-    onSuccess: (data) => {
-      setHash(data.hash);
-
-      handlePendingTx({
-        hash: _.get(data, 'hash'),
-        toastData: {
-          title: `Top Hat Unlinked!`,
-          description: `Successfully unlinked top hat #${prettyIdToIp(
-            topHatPrettyId,
-          )}`,
-        },
-      });
-
-      toast.info({
-        title: 'Transaction submitted',
-        description: 'Waiting for your transaction to be accepted...',
-      });
-    },
-    onError: (error) => {
-      if (error.name === 'UserRejectedRequestError') {
-        toast.error({
-          title: 'Signature rejected!',
-          description: 'Please accept the transaction in your wallet',
-        });
-      } else {
-        toast.error({
-          title: 'Error occurred!',
-          // description: 'Please accept the transaction in your wallet',
-        });
-      }
-    },
-  });
-
-  const { isLoading } = useWaitForTransaction({
-    hash,
   });
 
   return {
