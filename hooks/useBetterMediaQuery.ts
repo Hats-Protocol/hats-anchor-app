fa2c7c8da03625263700de858f6428dc40e14d17
Scott Herren <me@stwoh.com>
2023-08-10 10:03:49 -0500

    fix tree render

---
+++ hooks/useBetterMediaQuery.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+function useBetterMediaQuery(mediaQueryString: string) {
+  const [matches, setMatches] = useState<boolean>();
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(mediaQueryString);
+    const listener = () => setMatches(!!mediaQueryList.matches);
+    listener();
+    mediaQueryList.addEventListener('change', listener); // updated from .addListener
+    return () => mediaQueryList.removeEventListener('change', listener); // updated from .removeListener
+  }, [mediaQueryString]);
+
+  return matches;
+}
+
+export default useBetterMediaQuery;
