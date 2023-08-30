acc878042f7f620f01aa2ce0de7d2e4eb63a4aaa
vidvidvid <weetopol@gmail.com>
2023-08-30 20:57:30 +0200

    refactor useMulticallCallData

--- lib/hats.ts
+++ lib/hats.ts
@@ -3,8 +3,21 @@ import { hatIdDecimalToIp } from '@hatsprotocol/sdk-v1-core';
 import _ from 'lodash';
 import { Hex } from 'viem';
 
-import { defaultHat, MUTABILITY } from '@/constants';
-import { FormData, Hierarchy, IControls, IHat, InputObject } from '@/types';
+import {
+  defaultHat,
+  FALLBACK_ADDRESS,
+  MUTABILITY,
+  TRIGGER_OPTIONS,
+} from '@/constants';
+import {
+  FormData,
+  FormDataDetails,
+  Hierarchy,
+  IControls,
+  IHat,
+  InputObject,
+} from '@/types';
+import { handleDetailsPin } from './ipfs';
 
 export const calculateNextChildId = (id: string, hatsData: IHat[]) => {
   const children = _.filter(hatsData, ['admin.id', id]);
@@ -323,4 +336,214 @@ export const translateDrafts = ({
   });
 
   return _.filter(extendDrafts, (x) => x) as IHat[];
+};
+
+const hasDetailsChanged = ({
+  name,
+  description,
+  guilds,
+  responsibilities,
+  authorities,
+  isEligibilityManual,
+  revocationsCriteria,
+  isToggleManual,
+  deactivationsCriteria,
+}: Partial<FormDataDetails>) => {
+  return (
+    name ||
+    description ||
+    _.gt(_.size(guilds), 0) ||
+    _.gt(_.size(responsibilities), 0) ||
+    _.gt(_.size(authorities), 0) ||
+    isEligibilityManual ||
+    _.gt(_.size(revocationsCriteria), 0) ||
+    isToggleManual ||
+    _.gt(_.size(deactivationsCriteria), 0)
+  );
+};
+
+export const processHatForCalls = async (
+  hat: any,
+  onchainHats?: IHat[],
+  chainId?: number,
+  hatsClient?: any,
+) => {
+  const calls = [] as Hex[];
+
+  const {
+    maxSupply,
+    eligibility,
+    toggle,
+    mutable,
+    imageUrl,
+    isEligibilityManual,
+    isToggleManual,
+    revocationsCriteria,
+    deactivationsCriteria,
+    name,
+    description,
+    guilds,
+    responsibilities,
+    authorities,
+    wearers,
+    id: hatId,
+  } = hat;
+
+  if (!hatId || !chainId) return [];
+
+  const detailsData = {
+    name,
+    description,
+    guilds: guilds || [],
+    responsibilities: _.reject(responsibilities, ['label', '']),
+    authorities: _.reject(authorities, ['label', '']),
+    eligibility: {
+      manual: isEligibilityManual === TRIGGER_OPTIONS.MANUALLY,
+      criteria: _.reject(revocationsCriteria, ['label', '']) || [],
+    },
+    toggle: {
+      manual: isToggleManual === TRIGGER_OPTIONS.MANUALLY,
+      criteria: _.reject(deactivationsCriteria, ['label', '']) || [],
+    },
+  };
+
+  if (!_.includes(_.map(onchainHats, 'id'), hatId)) {
+    const details = await handleDetailsPin({
+      chainId,
+      hatId,
+      newDetails: detailsData,
+    });
+    const newHatData = hatsClient?.createHatCallData({
+      admin: BigInt(getDefaultAdminId(hatId)),
+      details,
+      maxSupply: _.toNumber(maxSupply) || 1,
+      eligibility: eligibility || FALLBACK_ADDRESS,
+      toggle: toggle || FALLBACK_ADDRESS,
+      mutable: mutable === MUTABILITY.MUTABLE,
+      imageURI: imageUrl,
+    });
+    if (newHatData && newHatData.callData) {
+      calls.push(newHatData.callData);
+    }
+  } else {
+    if (
+      hasDetailsChanged({
+        name,
+        description,
+        guilds,
+        responsibilities,
+        authorities,
+        isEligibilityManual,
+        revocationsCriteria,
+        isToggleManual,
+        deactivationsCriteria,
+      })
+    ) {
+      const existingDetails = _.get(
+        _.find(onchainHats, ['id', hatId]),
+        'detailsObject.data',
+      );
+
+      const newCid = await handleDetailsPin({
+        chainId,
+        hatId,
+        newDetails: detailsData,
+        existingDetails,
+      });
+
+      const changeHatDetailsData = hatsClient?.changeHatDetailsCallData({
+        hatId: decimalId(hatId) as unknown as bigint,
+        newDetails: newCid,
+      });
+
+      if (changeHatDetailsData?.callData) {
+        calls.push(changeHatDetailsData.callData);
+      }
+    }
+
+    if (maxSupply) {
+      const changeHatMaxSupplyData = hatsClient?.changeHatMaxSupplyCallData({
+        hatId: decimalId(hatId) as unknown as bigint,
+        newMaxSupply: parseInt(maxSupply, 10),
+      });
+
+      if (changeHatMaxSupplyData?.callData) {
+        calls.push(changeHatMaxSupplyData.callData);
+      }
+    }
+
+    if (wearers) {
+      if (_.eq(_.size(wearers), 1)) {
+        const wearerAddress = _.get(_.first(wearers), 'address');
+        if (wearerAddress) {
+          const mintHatWearersData = hatsClient?.mintHatCallData({
+            hatId: decimalId(hatId) as unknown as bigint,
+            wearer: wearerAddress,
+          });
+
+          if (mintHatWearersData?.callData) {
+            calls.push(mintHatWearersData.callData);
+          }
+        }
+      } else {
+        const batchMintHatWearersData = hatsClient?.batchMintHatsCallData({
+          hatIds: Array(_.size(wearers)).fill(
+            decimalId(hatId),
+          ) as unknown as bigint[],
+          wearers: _.map(wearers, 'address'),
+        });
+
+        if (batchMintHatWearersData?.callData) {
+          calls.push(batchMintHatWearersData.callData);
+        }
+      }
+    }
+
+    if (eligibility) {
+      const changeHatEligibilityData = hatsClient?.changeHatEligibilityCallData(
+        {
+          hatId: decimalId(hatId) as unknown as bigint,
+          newEligibility: eligibility,
+        },
+      );
+
+      if (changeHatEligibilityData?.callData) {
+        calls.push(changeHatEligibilityData.callData);
+      }
+    }
+
+    if (toggle) {
+      const changeHatToggleData = hatsClient?.changeHatToggleCallData({
+        hatId: decimalId(hatId) as unknown as bigint,
+        newToggle: toggle,
+      });
+
+      if (changeHatToggleData?.callData) {
+        calls.push(changeHatToggleData.callData);
+      }
+    }
+
+    if (mutable) {
+      const makeHatImmutableData = hatsClient?.makeHatImmutableCallData({
+        hatId: decimalId(hatId) as unknown as bigint,
+      });
+
+      if (makeHatImmutableData?.callData) {
+        calls.push(makeHatImmutableData.callData);
+      }
+    }
+
+    if (imageUrl) {
+      const changeHatImageURIData = hatsClient?.changeHatImageURICallData({
+        hatId: decimalId(hatId) as unknown as bigint,
+        newImageURI: imageUrl,
+      });
+
+      if (changeHatImageURIData?.callData) {
+        calls.push(changeHatImageURIData.callData);
+      }
+    }
+  }
+
+  return calls;
 };
