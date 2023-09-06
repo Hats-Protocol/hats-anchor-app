236c952ff7999863f39d4486945865d6bc6c7bc5
scottrepreneur <me@stwoh.com>
2023-09-06 15:41:47 -0500

    fix infinite initial data

--- pages/trees/[chainId]/index.tsx
+++ pages/trees/[chainId]/index.tsx
@@ -8,22 +8,23 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import Layout from '@/components/Layout';
 import NetworkFilter from '@/components/NetworkFilter';
 import TreeCard from '@/components/TreeListCard';
-// import { fetchPaginatedTrees } from '@/gql/helpers';
+import { fetchPaginatedTrees } from '@/gql/helpers';
 import useImageURIs from '@/hooks/useImageURIs';
 import usePaginatedTreeList from '@/hooks/usePaginatedTreeList';
 import { mapWithChainId } from '@/lib/general';
 import { IHat, ITree } from '@/types';
 
 const Trees = ({
-  // trees: initialData,
+  trees: initialTrees,
   chainId,
 }: {
-  // trees: InfiniteData<ITree[]>;
+  trees: ITree[];
   chainId: number;
 }) => {
   const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
     usePaginatedTreeList({
       chainId,
+      initialData: initialTrees,
     });
 
   const trees = _.flatten(_.get(data, 'pages'));
@@ -109,14 +110,14 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   const chainId = _.get(context, 'params.chainId');
 
   try {
-    // const trees = await fetchPaginatedTrees(Number(chainId) || 1, 0, 40);
-    // console.log(trees);
+    const trees = await fetchPaginatedTrees(Number(chainId) || 1, 0, 40);
 
     return {
       props: {
         chainId,
-        trees: [],
+        trees,
       },
+      revalidate: 30,
     };
   } catch (error) {
     // eslint-disable-next-line no-console
