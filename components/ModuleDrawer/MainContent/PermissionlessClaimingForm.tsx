98f6dc988ed64c6249873fb5cc28e35a98e9c4af
vidvidvid <weetopol@gmail.com>
2023-09-21 18:57:58 +0200

    split MainContent into multiple components

---
+++ components/ModuleDrawer/MainContent/PermissionlessClaimingForm.tsx
@@ -0,0 +1,111 @@
+import { Icon, Stack, Text } from '@chakra-ui/react';
+import _ from 'lodash';
+import { useMemo } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import { BsBarChartLine, BsPersonAdd, BsPuzzle } from 'react-icons/bs';
+
+import RadioBox from '@/components/atoms/RadioBox';
+import Select from '@/components/atoms/Select';
+import FormRowWrapper from '@/components/FormRowWrapper';
+import { useTreeForm } from '@/contexts/TreeFormContext';
+import {
+  decimalId,
+  getAllParents,
+  idToPrettyId,
+  prettyIdToIp,
+} from '@/lib/hats';
+
+const PermissionlessClaimingForm = ({
+  localForm,
+}: {
+  localForm: UseFormReturn<any>;
+}) => {
+  const { onchainHats, treeToDisplay, topHatDetails, selectedHat } =
+    useTreeForm();
+  const adminHat = localForm.watch('adminHat');
+
+  const parentHats = useMemo(() => {
+    return getAllParents(selectedHat?.id, treeToDisplay);
+  }, [selectedHat, treeToDisplay]);
+
+  if (!onchainHats || !treeToDisplay) return null;
+
+  return (
+    <Stack spacing={12}>
+      <FormRowWrapper>
+        <Icon as={BsPersonAdd} boxSize={4} mt='2px' />
+        <RadioBox
+          name='Claimable For'
+          label='Hat ClaimIng'
+          subLabel='Should this hat be permissionlessly claimable by potential wearers who meet the requirements of the accountability module?'
+          localForm={localForm}
+          options={[
+            {
+              label: 'Yes',
+              value: 'Yes',
+            },
+            {
+              label: 'No — admin mint only',
+              value: 'No',
+            },
+          ]}
+        />
+      </FormRowWrapper>
+      <FormRowWrapper>
+        <Icon as={BsPuzzle} boxSize={4} mt='2px' />
+        <Stack>
+          <Select
+            name='adminHat'
+            label='ADMIN HAT'
+            subLabel='To enable permissionless claiming, give an admin hat in this tree to the new hatter contract. Must be a non-top hat admin of this hat.'
+            localForm={localForm}
+            placeholder='Select a hat in this tree'
+            defaultValue={undefined}
+            options={{
+              required: true,
+            }}
+          >
+            {_.map(parentHats, (id) => (
+              <option value={decimalId(id)} key={id}>
+                {prettyIdToIp(idToPrettyId(id))}
+              </option>
+            ))}
+          </Select>
+          {adminHat && (
+            <Text color='blackAlpha.600'>
+              Potential wearers will be able to claim this hat if they meet the
+              requirements in new module above.
+            </Text>
+          )}
+        </Stack>
+      </FormRowWrapper>
+      {selectedHat?.wearers === selectedHat?.maxSupply && (
+        <FormRowWrapper>
+          <Icon as={BsBarChartLine} boxSize={4} mt='2px' />
+          <Stack>
+            <RadioBox
+              name='increment'
+              label='Increment Max Wearers by 1'
+              subLabel='The admin hat you selected (2.3 — Builder Custodian) has no more available supply to mint. Do you want to increase the max wearers by 1 in order to mint this hat to the new hatter contract?'
+              localForm={localForm}
+              options={[
+                {
+                  label: `Yes — increase max wearers from ${
+                    selectedHat?.maxSupply
+                  } to ${Number(selectedHat?.maxSupply) + 1}`,
+                  value: 'Yes',
+                },
+                {
+                  label: 'No (cancel deployment)',
+                  value: 'No',
+                },
+              ]}
+            />
+          </Stack>
+        </FormRowWrapper>
+      )}
+    </Stack>
+  );
+};
+
+export default PermissionlessClaimingForm;
