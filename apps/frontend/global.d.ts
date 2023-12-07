b2163e0df6d295ccd2a06e89104cc7292b81a6f7
scottrepreneur <me@stwoh.com>
2023-12-07 17:18:57 -0600

    build/types

--- apps/frontend/global.d.ts
+++ apps/frontend/global.d.ts
@@ -2,8 +2,6 @@ declare module 'papaparse';
 declare module 'd3';
 declare module 'lodash';
 declare module '@next/bundle-analyzer';
-declare module '@chakra-ui/react';
-declare module 'viem';
 
 declare global {
   interface Window {
