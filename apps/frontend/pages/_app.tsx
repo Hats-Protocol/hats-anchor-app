7e4136f18489a09ddfc11f602e2e439efaee1e5d
scottrepreneur <me@stwoh.com>
2024-04-02 09:18:13 -0500

    fix analytics

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -21,16 +21,11 @@ import { chains, wagmiConfig } from 'utils';
 import { WagmiConfig } from 'wagmi';
 
 const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
-const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : undefined;
-const PUBLIC_HOST = HOST_URL || VERCEL_URL || 'http://localhost:4200';
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== 'undefined') {
   posthog.init(POSTHOG_KEY, {
-    api_host: `${HOST_URL}/ingest` || 'https://app.posthog.com',
+    api_host: `/ingest` || 'https://app.posthog.com',
     // Enable debug mode in development
     loaded: (p: { debug: () => void }) => {
       if (process.env.NODE_ENV === 'development') p.debug();
