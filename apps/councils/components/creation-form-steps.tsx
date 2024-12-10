d90cce31a8ebff7a16f5143df1035a59de148655
Ido <gershido@gmail.com>
2024-12-10 15:27:48 +0200

    make substeps clickable

--- apps/councils/components/creation-form-steps.tsx
+++ apps/councils/components/creation-form-steps.tsx
@@ -183,40 +183,52 @@ export function CreationFormSteps({
                   const isCurrent = subStep.id === currentSubStep;
 
                   return (
-                    <div
+                    <Link
                       key={subStep.id}
-                      className={`flex items-center gap-3 border-l-[2px] ${isCompleted ? 'border-l-blue-500' : 'border-l-gray-200'}`}
+                      href={`/councils/new/selection?subStep=${subStep.id}&draftId=${draftId}`}
                     >
                       <div
-                        className={`my-1 ml-4 flex h-6 w-6 items-center justify-center rounded-full ${isCurrent ? 'border border-blue-500 bg-blue-100' : 'border border-gray-200 bg-white'}`}
+                        className={`flex items-center gap-3 border-l-[2px] ${
+                          isCompleted
+                            ? 'border-l-blue-500'
+                            : 'border-l-gray-200'
+                        }`}
                       >
-                        {isCompleted ? (
-                          <svg
-                            width='44'
-                            height='44'
-                            viewBox='0 0 44 44'
-                            fill='none'
-                            xmlns='http://www.w3.org/2000/svg'
-                            className='text-white'
-                          >
-                            <g id='check-circle-fill'>
-                              <path
-                                id='Subtract'
-                                d='M44 22C44 34.1503 34.1503 44 22 44C9.84974 44 0 34.1503 0 22C0 9.84974 9.84974 0 22 0C34.1503 0 44 9.84974 44 22ZM33.0834 13.6666C32.2779 12.8611 30.972 12.8611 30.1666 13.6666C30.1471 13.686 30.1289 13.7066 30.1119 13.7283L20.5628 25.896L14.8057 20.1389C14.0002 19.3334 12.6943 19.3334 11.8888 20.1389C11.0834 20.9443 11.0834 22.2502 11.8888 23.0557L19.1666 30.3334C19.972 31.1389 21.2779 31.1389 22.0834 30.3334C22.1013 30.3155 22.1183 30.2966 22.1341 30.2768L33.1127 16.5535C33.8887 15.746 33.879 14.4622 33.0834 13.6666Z'
-                                fill='#2B6CB0'
-                              />
-                            </g>
-                          </svg>
-                        ) : (
-                          <span className='text-sm text-gray-500'>
-                            {subIndex + 1}
-                          </span>
-                        )}
+                        <div
+                          className={`my-1 ml-4 flex h-6 w-6 items-center justify-center rounded-full ${
+                            isCurrent
+                              ? 'border border-blue-500 bg-blue-100'
+                              : 'border border-gray-200 bg-white'
+                          }`}
+                        >
+                          {isCompleted ? (
+                            <svg
+                              width='44'
+                              height='44'
+                              viewBox='0 0 44 44'
+                              fill='none'
+                              xmlns='http://www.w3.org/2000/svg'
+                              className='text-white'
+                            >
+                              <g id='check-circle-fill'>
+                                <path
+                                  id='Subtract'
+                                  d='M44 22C44 34.1503 34.1503 44 22 44C9.84974 44 0 34.1503 0 22C0 9.84974 9.84974 0 22 0C34.1503 0 44 9.84974 44 22ZM33.0834 13.6666C32.2779 12.8611 30.972 12.8611 30.1666 13.6666C30.1471 13.686 30.1289 13.7066 30.1119 13.7283L20.5628 25.896L14.8057 20.1389C14.0002 19.3334 12.6943 19.3334 11.8888 20.1389C11.0834 20.9443 11.0834 22.2502 11.8888 23.0557L19.1666 30.3334C19.972 31.1389 21.2779 31.1389 22.0834 30.3334C22.1013 30.3155 22.1183 30.2966 22.1341 30.2768L33.1127 16.5535C33.8887 15.746 33.879 14.4622 33.0834 13.6666Z'
+                                  fill='#2B6CB0'
+                                />
+                              </g>
+                            </svg>
+                          ) : (
+                            <span className='text-sm text-gray-500'>
+                              {subIndex + 1}
+                            </span>
+                          )}
+                        </div>
+                        <span className='text-sm text-gray-600'>
+                          {subStep.label}
+                        </span>
                       </div>
-                      <span className='text-sm text-gray-600'>
-                        {subStep.label}
-                      </span>
-                    </div>
+                    </Link>
                   );
                 })}
               </div>
