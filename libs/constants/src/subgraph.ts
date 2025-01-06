dc4ee8b489f68bf2e18948c2a88d6ba7b8604c5a
Ido <gershido@gmail.com>
2025-01-06 17:50:45 +0200

    update ancillary subgraphs

--- libs/constants/src/subgraph.ts
+++ libs/constants/src/subgraph.ts
@@ -38,12 +38,12 @@ export default NETWORK_ENDPOINTS;
 export const ANCILLARY_API_URL: {
   [key in SupportedChains]: string | undefined;
 } = {
-  1: studioUrl('hats-v1-ethereum-ancillary', 'v0.0.22'),
-  10: studioUrl('hats-v1-optimism-ancillary', 'v0.0.22'),
-  100: studioUrl('hats-v1-gnosis-chain-ancillary', 'v0.0.22'),
-  137: studioUrl('hats-v1-polygon-ancillary', 'v0.0.22'),
-  8453: studioUrl('hats-v1-base-ancillary', 'v0.0.23'),
-  42161: studioUrl('hats-v1-arbitrum-ancillary', 'v0.0.22'),
-  42220: studioUrl('hats-v1-celo-ancillary', 'v0.0.23'),
-  11155111: studioUrl('hats-v1-sepolia-ancillary', 'v0.0.23'),
+  1: studioUrl('hats-v1-ethereum-ancillary', 'v0.0.23'),
+  10: studioUrl('hats-v1-optimism-ancillary', 'v0.0.23'),
+  100: studioUrl('hats-v1-gnosis-chain-ancillary', 'v0.0.23'),
+  137: studioUrl('hats-v1-polygon-ancillary', 'v0.0.23'),
+  8453: studioUrl('hats-v1-base-ancillary', 'v0.0.24'),
+  42161: studioUrl('hats-v1-arbitrum-ancillary', 'v0.0.23'),
+  42220: studioUrl('hats-v1-celo-ancillary', 'v0.0.24'),
+  11155111: studioUrl('hats-v1-sepolia-ancillary', 'v0.0.24'),
 };
