8f0861afdcc30c2c6fa23bf4a4797457af94d3a7
scottrepreneur <me@stwoh.com>
2025-02-13 13:50:31 -0600

    initial sentry

--- apps/councils/next.config.mjs
+++ apps/councils/next.config.mjs
@@ -1,5 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { composePlugins, withNx } from '@nx/next';
+import { withSentryConfig } from '@sentry/nextjs';
+
+const SENTRY_WEBPACK_PLUGIN_OPTIONS = {
+  silent: true, // Can be used to suppress logs
+
+  org: process.env.NEXT_PUBLIC_SENTRY_ORG,
+  project: process.env.NEXT_PUBLIC_SENTRY_PROJECT,
+};
+
+const SENTRY_OPTIONS = {
+  // Sentry Next.js options here
+  // widenClientFileUpload: true,
+  // transpileClientSDK: true,
+  // tunnelRoute: '/monitoring',
+  // hideSourceMaps: true,
+  disableLogger: true, // Reduce bundle size
+  // automaticVercelMonitors: true,
+};
 
 /**
  * @type {import('next').NextConfig}
@@ -22,6 +40,7 @@ const nextConfig = {
   },
 
   experimental: {
+    instrumentationHook: true,
     optimizePackageImports: [
       // external pkgs
       '@rainbow-me/rainbowkit',
@@ -41,6 +60,15 @@ const nextConfig = {
   },
 };
 
-const plugins = [withNx];
+// Use withSentryConfig to wrap the next config
+const sentryEnhancedConfig = (passedConfig) =>
+  withSentryConfig(passedConfig, SENTRY_WEBPACK_PLUGIN_OPTIONS, SENTRY_OPTIONS);
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  // Sentry should be the last plugin to wrap all others
+  sentryEnhancedConfig,
+];
 
 export default composePlugins(...plugins)(nextConfig);
