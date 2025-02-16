fe02ab70e4f4e1b34da5484187e5a95b2d46383f
jonathanprozzi <jonathanprozzi@gmail.com>
2025-02-15 22:09:40 -0500

    Addresses feedback by removing potential race condition and fail states in auth and adds useEffect cleanup for the ref

--- libs/hooks/src/use-auth-guard.ts
+++ libs/hooks/src/use-auth-guard.ts
@@ -2,10 +2,12 @@
 
 import { usePrivy } from '@privy-io/react-auth';
 import { useEffect, useRef } from 'react';
+import { logger } from 'utils';
 
 export const useAuthGuard = () => {
   const { login, user, authenticated, ready, isModalOpen } = usePrivy();
   const hasTriggeredLogin = useRef(false);
+  const isMounted = useRef(false);
 
   useEffect(() => {
     if (!ready) return;
@@ -17,12 +19,23 @@ export const useAuthGuard = () => {
     const handleAuth = async () => {
       if (!authenticated && !hasTriggeredLogin.current) {
         hasTriggeredLogin.current = true;
-        login();
+        try {
+          await login();
+        } catch (error) {
+          logger.error(error);
+          hasTriggeredLogin.current = false;
+        }
         return;
       }
     };
 
-    handleAuth();
+    if (!isMounted.current) {
+      isMounted.current = true;
+      handleAuth();
+    }
+    return () => {
+      isMounted.current = false;
+    };
   }, [ready, authenticated, login, isModalOpen]);
 
   return {
