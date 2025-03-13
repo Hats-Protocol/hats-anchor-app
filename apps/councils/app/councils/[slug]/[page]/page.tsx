a68a7e4b8aa5cbe6fee4017a172f16393a538f6f
jonathanprozzi <jonathanprozzi@gmail.com>
2025-03-13 17:13:52 -0400

    Adds page level redirect for invalid pages on /councils/[slug]/[pages] -- these now redirect to /members for now

--- apps/councils/app/councils/[slug]/[page]/page.tsx
+++ apps/councils/app/councils/[slug]/[page]/page.tsx
@@ -1,16 +1,23 @@
 import { ModuleChainClaim } from 'modules-ui';
+import { redirect } from 'next/navigation';
 import { parseCouncilSlug } from 'utils';
 import { Hex } from 'viem';
 
 import { CouncilsDevInfo } from '../../../../components/councils-dev-info';
 import { ManagePage } from '../../../../components/manage-page';
 import { MembersPage } from '../../../../components/members-page';
-import { SafeAssetsPage } from '../../../../components/safe-assets-page';
+// import { SafeAssetsPage } from '../../../../components/safe-assets-page';
+
+const VALID_PAGES = ['transactions', 'join', 'manage', 'members', 'dev', 'assets'];
 
 const CouncilDetails = async ({ params }: { params: Promise<{ slug: string; page: string }> }) => {
   const { slug, page } = await params;
-  // TODO identifier could be ID in database, slug or chainId/hsg
   const { chainId, address } = parseCouncilSlug(slug);
+
+  if (!VALID_PAGES.includes(page)) {
+    // Redirect invalid pages to members instead of using the default page
+    redirect(`/councils/${slug}/members`);
+  }
 
   if (page === 'transactions') {
     return <div>Transactions</div>;
@@ -32,10 +39,8 @@ const CouncilDetails = async ({ params }: { params: Promise<{ slug: string; page
     return <CouncilsDevInfo slug={slug} />;
   }
 
-  if (!address) return null;
-
-  // Default is assets
-  return <SafeAssetsPage chainId={chainId ?? 11155111} hsg={address as Hex} />;
+  // this should never be reached due to the VALID_PAGES check above, but incase it does we handle it here with a redirect
+  redirect(`/councils/${slug}/members`);
 };
 
 export default CouncilDetails;
