83a38ea80d126d22eb283cd665efcd8fefa074de
jonathanprozzi <jonathanprozzi@gmail.com>
2025-03-20 11:04:35 -0400

    Adds new use-safe-registered-events hook for ancillary subgraph call

---
+++ libs/hooks/src/use-safe-registered-events.ts
@@ -0,0 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+import { get } from 'lodash';
+import { ExtendedHSGV2, SupportedChains } from 'types';
+import { ancillarySubgraphClient } from 'utils';
+import { Hex } from 'viem';
+
+const SAFE_REGISTERED_EVENTS_QUERY = gql`
+  query SafeRegisteredEvents($hsg: String!) {
+    hatsSignerGateV2S(where: { hsg: $hsg }) {
+      id
+      signer
+      timestamp
+      hatId
+      hsg {
+        id
+        safe
+      }
+    }
+  }
+`;
+
+const fetchSafeRegisteredEvents = async (hsg: Hex, chainId: number | undefined) => {
+  if (!chainId) return Promise.resolve(null);
+
+  const client = ancillarySubgraphClient(chainId as SupportedChains);
+  if (!client) return Promise.resolve(null);
+  const variables = { hsg };
+  const result = await client.request(SAFE_REGISTERED_EVENTS_QUERY, variables);
+
+  return Promise.resolve((get(result, 'hatsSignerGateV2S') || null) as ExtendedHSGV2[] | null);
+};
+
+const useSafeRegisteredEvents = ({ hsg, chainId }: UseSafeRegisteredEventsProps) => {
+  return useQuery({
+    queryKey: ['safeRegisteredEvents', { hsg, chainId }],
+    queryFn: () => fetchSafeRegisteredEvents(hsg, chainId),
+    enabled: !!hsg && !!chainId,
+  });
+};
+
+interface UseSafeRegisteredEventsProps {
+  hsg: Hex;
+  chainId: number | undefined;
+}
+
+export { useSafeRegisteredEvents };
