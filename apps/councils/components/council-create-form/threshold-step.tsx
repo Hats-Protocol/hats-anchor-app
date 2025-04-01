6c3caed5ea24a22601b8e8458e97785439450a5b
jonathanprozzi <jonathanprozzi@gmail.com>
2025-04-01 19:41:58 -0400

    Adds Skeleton to the Signer Threshold step

--- apps/councils/components/council-create-form/threshold-step.tsx
+++ apps/councils/components/council-create-form/threshold-step.tsx
@@ -32,30 +32,19 @@ export function ThresholdStep({ onNext, draftId }: StepProps) {
     return (
       <div className='flex h-full flex-col'>
         <div className='flex-1 space-y-6'>
-          {/* Header */}
           <div className='space-y-2'>
             <Skeleton className='h-8 w-48' />
             <Skeleton className='h-4 w-32' />
           </div>
 
-          {/* SignerThresholdSubForm skeleton */}
           <div className='space-y-6'>
-            {/* Radio group */}
             <div className='space-y-4'>
-              <Skeleton className='h-5 w-64' />
-              <div className='flex space-y-2'>
-                <div className='flex items-center gap-3'>
-                  <Skeleton className='h-4 w-4 rounded-full' />
-                  <Skeleton className='h-4 w-48' />
-                </div>
-                <div className='flex items-center gap-3'>
-                  <Skeleton className='h-4 w-4 rounded-full' />
-                  <Skeleton className='h-4 w-64' />
-                </div>
+              <div className='flex items-center gap-2'>
+                <Skeleton className='h-5 w-64' />
+                <Skeleton className='h-5 w-64' />
               </div>
             </div>
 
-            {/* Number inputs */}
             <div className='space-y-6'>
               <div className='space-y-2'>
                 <Skeleton className='h-5 w-44' />
