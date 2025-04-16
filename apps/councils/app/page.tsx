18441e5317ec4247aebdaab3941d8434a3f84e88
scottrepreneur <me@stwoh.com>
2025-04-16 16:14:58 -0500

    remove orgs flag

--- apps/councils/app/page.tsx
+++ apps/councils/app/page.tsx
@@ -1,16 +1,7 @@
-import posthog from 'posthog-js';
-
-import { CouncilListPage } from '../components/council-list-page';
 import { CouncilListPageOrgs } from '../components/councils-list-page-orgs';
 
 const Home = () => {
-  const isOrgs = posthog.isFeatureEnabled('orgs') || process.env.NODE_ENV !== 'production';
-
-  if (isOrgs) {
-    return <CouncilListPageOrgs />;
-  }
-
-  return <CouncilListPage />;
+  return <CouncilListPageOrgs />;
 };
 
 export default Home;
