23384f640222d0d11097e2f2cd2f822a7db3e664
scottrepreneur <me@stwoh.com>
2026-01-29 07:21:38 -0800

    fix hat deactivation

--- apps/frontend/app/buidl/active/page.tsx
+++ apps/frontend/app/buidl/active/page.tsx
@@ -6,7 +6,7 @@ const MassDeactivationPage = () => {
   return (
     <div className='mx-auto flex max-w-screen-md flex-col gap-4 pt-28'>
       <div className='flex justify-center'>
-        <h2 className='text-2xl font-bold'>Mass Activate/Deactivate Hats</h2>
+        <h2 className='text-2xl font-bold'>Mass Deactivate Hats</h2>
       </div>
 
       <div>
